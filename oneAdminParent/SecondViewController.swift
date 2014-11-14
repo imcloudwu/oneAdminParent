c3563cf877c226aeefb7c0c2d7cd86c4a80b6920
Cloud <imcloudwu@gmail.com>
2014-11-14 16:43:37 +0800

    Initial Commit

---
+++ oneAdminParent/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  oneAdminParent
+//
+//  Created by Cloud on 11/14/14.
+//  Copyright (c) 2014 ischool. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
